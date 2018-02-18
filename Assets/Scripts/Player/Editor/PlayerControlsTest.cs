264bfa7c7700e80fbe88e7b8fe089b0f7f22b90e
Nick Cipollo <njc1115@gmail.com>
2018-02-18 15:42:49 -0500

    Add PlayerControls class and tests.

---
+++ Assets/Scripts/Player/Editor/PlayerControlsTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerControlsTest {
+
+	private PlayerControls playerControls;
+
+	[SetUp]
+	public void SetUp() {
+		playerControls = new PlayerControls("_P1");
+	}
+
+	[Test]
+	public void Horizontal() {
+		Assert.AreEqual("Horizontal_P1", playerControls.Horizontal);
+	}
+
+	[Test]
+	public void Jump() {
+		Assert.AreEqual("Jump_P1", playerControls.Jump);
+	}
+
+	[Test]
+	public void Fire1() {
+		Assert.AreEqual("Fire1_P1", playerControls.Fire1);
+	}
+
+	[Test]
+	public void Fire2() {
+		Assert.AreEqual("Fire2_P1", playerControls.Fire2);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls {
+    public string Horizontal { get; }
+	public string Jump { get; }
+	public string Fire1 { get; }
+	public string Fire2 { get; }
+
+    public PlayerControls(string playerSuffix) {
+		Horizontal = $"Horizontal{playerSuffix}";
+		Jump = $"Jump{playerSuffix}";
+		Fire1 = $"Fire1{playerSuffix}";
+		Fire2 = $"Fire2{playerSuffix}";
+    }
+}
