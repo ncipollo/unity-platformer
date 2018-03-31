49460aa0dfc001f27271000637206f66e288ab2f
Nick Cipollo <njc1115@gmail.com>
2018-03-31 14:01:52 -0400

    Add player status bars.

---
+++ Assets/Scripts/UI/Player Status/UIPlayerStatusBars.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerStatusBars : MonoBehaviour {
+	public Image hpFill;
+	public Image staminaFill;
+
+	public float hp = 1.0f;
+	public float stamina = 1.0f;
+	
+	void Update () {
+		hpFill.fillAmount = hp;
+		staminaFill.fillAmount = stamina;
+	}
+}
